e5c17845307735e1f57ea37bfafea8566022e2fb
Gabriel_dev <gabrielserafim401@gmail.com>
2026-04-17 13:57:17 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
+const hShadow = document.querySelector('#h-shadow');
+const vShadow = document.querySelector('#v-shadow');
+const preview = document.querySelector('#preview');
+const codeOutput = document.querySelector('#code-output');
 
+function updateShadow() {
+   
+    const h = hShadow.value;
+    const v = vShadow.value;
+    
+
+    const shadowString = `${h}px ${v}px 15px rgba(0, 0, 0, 0.5)`;
+    
+   
+    preview.style.boxShadow = shadowString;
+    codeOutput.innerText = `box-shadow: ${shadowString};`;
+}
+
+
+hShadow.addEventListener('input', updateShadow);
+vShadow.addEventListener('input', updateShadow);
+
+
+updateShadow();
